6ac37cd5d4101a8209054c90a98816588b2a31a4
KaBaNChik <139145161+KaBaN5008@users.noreply.github.com>
2026-09-14 14:24:11 +0300

    Update lab1.py

--- lab1.py
+++ lab1.py
@@ -2,7 +2,7 @@ import csv
 from io import StringIO
 from typing import Optional
 
-data = """date,item,quantity,price
+DATA = """date,item,quantity,price
 2026-09-01,Apple,10,2.5
 2026-09-02,Banana,5,3
 2026-09-03,Orange,8,4
@@ -10,7 +10,7 @@ data = """date,item,quantity,price
 
 # Уровень 1
 def parse_csv(data: str) -> list[dict]:
-    return list(csv.DirectReader(StringIO(data)))
+    return list(csv.DictReader(StringIO(data)))
 
 
 # Уровень 2
@@ -30,10 +30,3 @@ def top_item(rows: list[dict]) -> Optional[dict]:
         rows,
         key=lambda row: int(row["quantity"]) * float(row["price"])
     )
-
-
-rows = parse_csv(data)
-
-print(rows)
-print(compute_revenue(rows))
-print(top_item(rows))
